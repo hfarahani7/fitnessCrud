1e015d4ae35a5a6a7187290d93f59bcd7f54e21a
hfarahani7 <hfarahani7@gmail.com>
2022-11-21 20:45:51 -0500

    adding models

--- fitnessCrud/trainerPicker/models.py
+++ fitnessCrud/trainerPicker/models.py
@@ -2,19 +2,26 @@ from django.db import models
 from datetime import datetime
 from django.contrib.auth.models import User
 
-class DateTimeManager(models.Manager):
-    def getTime(self):
-        return(self.strfTime("%m%d%Y"))
-    def getDate(self):
-        return(self.strfTime("%H%M%S"))
+# class DateTimeManager(models.Manager):
+#     def getTime(self):
+#         return(self.strfTime("%m%d%Y"))
+#     def getDate(self):
+#         return(self.strfTime("%H%M%S"))
 
 class TrainingSession(models.Model):
     title = models.CharField(max_length=15)
-    description = models.CharField(max_length=120)
-    dateTime = models.DateTimeField()
-    date = DateTimeManager.getDate(dateTime)
-    time = DateTimeManager.getTime(dateTime)
-    instructorID = models.ForeignKey(User, on_delete=models.CASCADE)
-    maxParticipants = models.IntegerField()
+    description = models.CharField(max_length=120, null=True)
+    dateTime = models.DateTimeField(null=True)
+    # date = DateTimeManager.getDate(dateTime)
+    # time = DateTimeManager.getTime(dateTime)
+    instructorID = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    maxParticipants = models.IntegerField(null=True)
+    participants = 
+
+    def __str__(self):
+        return self.title
+
+    def isFull(self):
+        return(self.maxParticipants < len(self.participants))
 
 # Create your models here.
