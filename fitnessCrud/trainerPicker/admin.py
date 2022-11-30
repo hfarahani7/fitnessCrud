9e43635d11b28c7e51e6da33e967df50f47d33f9
hfarahani7 <hfarahani7@gmail.com>
2022-11-29 19:27:39 -0500

    DB almost done

--- fitnessCrud/trainerPicker/admin.py
+++ fitnessCrud/trainerPicker/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import GroupClass, Session
+from .models import GroupClass, Session, TraineeData, TrainerData
 
 admin.site.register(GroupClass)
 admin.site.register(Session)
+admin.site.register(TraineeData)
+admin.site.register(TrainerData)
 # Register your models here.
--- fitnessCrud/trainerPicker/models.py
+++ fitnessCrud/trainerPicker/models.py
@@ -2,30 +2,24 @@ from django.db import models
 from datetime import datetime
 from django.contrib.auth.models import User
 
-# class Trainer(User):
-#     firstName = models.TextField()
-#     lastName = models.TextField()
-#     bio = models.TextFiel(max_length=120)
-#     age = models.PositiveIntegerField()
+class TrainerData(models.Model):
+    id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    age = models.PositiveIntegerField(null=True)
+    gender = models.CharField(
+        choices = [
+            ('Male', 'Male'),
+            ('Female', 'Female'),
+            ('Other', 'Other')
+        ],
+        default='Male',
+        max_length=10
+    )
+    bio = models.CharField(max_length=120)
 
-#     def __str__(self):
-#         return (self.firstName + " " + self.lastName)
-
-# class Trainee(models.User):
-#     firstName = models.TextField()
-#     lastName = models.TextField()
-#     age = models.PositiveIntegerField()
-#     weight = models.PositiveIntegerField()
-#     heightInches = models.PositiveIntegerField()
-
-#     def __str__(self):
-#         return (self.firstName + " " + self.lastName)
-
-# class DateTimeManager(models.Manager):
-#     def getTime(self):
-#         return(self.strfTime("%m%d%Y"))
-#     def getDate(self):
-#         return(self.strfTime("%H%M%S"))
+class TraineeData(models.Model):
+    id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    weight = models.PositiveIntegerField()
+    height = models.PositiveIntegerField()
 
 class GroupClass(models.Model):
     title = models.CharField(max_length=15)
@@ -33,9 +27,8 @@ class GroupClass(models.Model):
     dateTime = models.DateTimeField(null=True)
     # date = DateTimeManager.getDate(dateTime)
     # time = DateTimeManager.getTime(dateTime)
-    instructorID = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    trainerID = models.ForeignKey(TrainerData, on_delete=models.CASCADE, null=True)
     maxParticipants = models.IntegerField(null=True)
-    participants = models.JSONField(null=True) 
 
     def __str__(self):
         return self.title
@@ -44,6 +37,6 @@ class GroupClass(models.Model):
         return(self.maxParticipants < len(self.participants))
 
 class Session(models.Model):
-    trainerID = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=False, )
-    traineeID = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True)
+    traineeID = models.ManyToManyField(TraineeData)
     classID = models.ForeignKey(GroupClass, on_delete=models.DO_NOTHING)
+    
