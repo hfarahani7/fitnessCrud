b17f3aa37ddd38f0d3de7d1a63c883c5ffee3568
Dylan Jaipersaud <dyjaipersaud@gmail.com>
2022-12-04 16:14:07 -0500

    Added CORS to accept localhost:8080. Run 'python -m pip install django-cors-headers' if there are issues with it.

--- fitnessCrud/fitnessCrud/settings.py
+++ fitnessCrud/fitnessCrud/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'trainerPicker.apps.TrainerpickerConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -53,6 +55,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'fitnessCrud.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080',
+]
 
 TEMPLATES = [
     {
