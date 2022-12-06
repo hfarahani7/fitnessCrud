bb0bfd25138bf0549e26843fc0fc65bf1d73dcef
henry farahani <hfarahani7@gmail.com>
2022-12-06 01:07:57 -0500

    Change settings to token based authentication

--- fitnessCrud/fitnessCrud/settings.py
+++ fitnessCrud/fitnessCrud/settings.py
@@ -138,6 +138,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication'
+        'rest_framework.authentication.TokenAuthentication'
     ]
 }
